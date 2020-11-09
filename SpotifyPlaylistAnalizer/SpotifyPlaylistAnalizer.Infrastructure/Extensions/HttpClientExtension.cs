53c8e06472f7ed31f47edc9b920e4b6f55c5ab86
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-09 22:28:40 +0200

    added http client extension

---
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Infrastructure/Extensions/HttpClientExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SpotifyPlaylistAnalizer.Infrastructure.Extensions
+{
+    public static class HttpClientExtension
+    {
+        public static async Task<T> ReadAsJsonAsync<T>(this HttpContent content)
+        {
+            using (var stream = await content.ReadAsStreamAsync())
+            {
+                T t = await JsonSerializer.DeserializeAsync<T>(stream);
+                return t;
+            }
+        }
+    }
+}
