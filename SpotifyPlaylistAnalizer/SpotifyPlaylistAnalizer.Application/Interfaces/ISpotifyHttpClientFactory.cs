a1e9dd0ed4a566534d496350b80312f00c9114ae
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-09 22:39:15 +0200

    added spotify hhtp client factory

---
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Application/Interfaces/ISpotifyHttpClientFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotifyPlaylistAnalizer.Application.Interfaces
+{
+    public interface ISpotifyHttpClientFactory
+    {
+        Task<HttpClient> CreateHttpClient();
+    }
+}
---
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Infrastructure/Services/SpotifyHttpClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using SpotifyPlaylistAnalizer.Application.Interfaces;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SpotifyPlaylistAnalizer.Infrastructure.Services
+{
+    public class SpotifyHttpClientFactory : ISpotifyHttpClientFactory
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ISpotifyTokenService _spotifyTokenService;
+
+        public SpotifyHttpClientFactory(HttpClient httpClient, ISpotifyTokenService spotifyTokenService)
+        {
+            httpClient.BaseAddress = new System.Uri("https://api.spotify.com/v1/");
+            _httpClient = httpClient;
+            _spotifyTokenService = spotifyTokenService;
+        }
+
+        public async Task<HttpClient> CreateHttpClient()
+        {
+            var token = await _spotifyTokenService.IssueToken();
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            return _httpClient;
+        }
+    }
+}
