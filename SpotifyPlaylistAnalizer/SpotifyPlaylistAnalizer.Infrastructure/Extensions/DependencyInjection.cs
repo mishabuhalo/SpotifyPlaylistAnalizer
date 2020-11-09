fd19b4947b0b044635f3a88e67658d14d9be9e81
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-09 23:34:34 +0200

    registered infrastructure in web api

--- SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Infrastructure/Extensions/DependencyInjection.cs
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SpotifyPlaylistAnalizer.Application.Interfaces;
+using SpotifyPlaylistAnalizer.Application.Models;
 using SpotifyPlaylistAnalizer.Infrastructure.Services;
 
 namespace SpotifyPlaylistAnalizer.Infrastructure.Extensions
@@ -9,8 +10,10 @@ namespace SpotifyPlaylistAnalizer.Infrastructure.Extensions
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.Configure<SpotifyTokenService>(configuration.GetSection("SpotifyTokenSettings"));
+            services.Configure<SpotifyTokenSettings>(configuration.GetSection("SpotifyTokenSettings"));
             services.AddTransient<ISpotifyTokenService, SpotifyTokenService>();
+            services.AddHttpClient<ISpotifyHttpClientFactory, SpotifyHttpClientFactory>();
+            services.AddTransient<ISpotifyService, SpotifyService>();
             return services;
         }
     }
--- SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer/Startup.cs
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SpotifyPlaylistAnalizer.Infrastructure.Extensions;
 
 namespace SpotifyPlaylistAnalizer
 {
@@ -28,6 +22,7 @@ namespace SpotifyPlaylistAnalizer
         {
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddInfrastructure(_configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
