24ff6ea0036f6042ae68c0c6e3bdc8fbd5df0a7c
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-09 20:39:03 +0200

    use default routing

--- SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer/Startup.cs
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer/Startup.cs
@@ -12,13 +12,10 @@ namespace SpotifyPlaylistAnalizer
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -30,10 +27,7 @@ namespace SpotifyPlaylistAnalizer
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
