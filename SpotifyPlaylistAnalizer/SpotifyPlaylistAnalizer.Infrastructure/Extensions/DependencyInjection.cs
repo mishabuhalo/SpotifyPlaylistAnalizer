59caa53a0113e232e6c76e06182196d5c11acf44
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-09 22:29:22 +0200

    added di extension and registered spotify services

---
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpotifyPlaylistAnalizer.Application.Interfaces;
+using SpotifyPlaylistAnalizer.Infrastructure.Services;
+
+namespace SpotifyPlaylistAnalizer.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<SpotifyTokenService>(configuration.GetSection("SpotifyTokenSettings"));
+            services.AddTransient<ISpotifyTokenService, SpotifyTokenService>();
+            return services;
+        }
+    }
+}
