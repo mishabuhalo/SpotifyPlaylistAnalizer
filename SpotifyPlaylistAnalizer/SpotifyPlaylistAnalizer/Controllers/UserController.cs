6406834dfa6f312302a64e98d75e42689b371cf2
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-11-16 21:57:19 +0200

    added user controller

---
+++ SpotifyPlaylistAnalizer/SpotifyPlaylistAnalizer/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpotifyPlaylistAnalizer.Application.SpotifyAPI.User;
+
+namespace SpotifyPlaylistAnalizer.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUserInfo(string userId)
+        {
+            dynamic result = JsonSerializer.Deserialize<dynamic>(await Mediator.Send(new GetUserInfoQuery() { UserId = userId}));
+
+            return Ok(result);
+        }
+    }
+}
